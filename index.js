f81369ee53ce7f1c6a2c32d076eb4537c38a0a63
MdSharib <mdsharib15041998@gmail.com>
2023-07-05 10:35:30 +0530

    added basic styling

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const form = document.getElementById("my-form");
 const studentInputName = document.getElementById("name");
 const studentInputAge = document.getElementById("age");
@@ -13,202 +12,190 @@ const search = document.getElementById("search");
 let studentDetails = [];
 let row = {};
 
-
-
-
-// search bar 
+// search bar
 const searchHandler = (ev) => {
-   const curr = ev.target.value.toLowerCase();
-//    console.log(curr);
-   const filteredData = studentDetails.filter((val) => {
+  const curr = ev.target.value.toLowerCase();
+  //    console.log(curr);
+  const filteredData = studentDetails.filter((val) => {
     const stName = val["name"].toLowerCase();
     const stEmail = val["email"].toLowerCase();
     const stDegree = val["degree"].toLowerCase();
-     if(stName.includes(curr) || stEmail.includes(curr) || stDegree.includes(curr)){
-        // console.log("from filter fn " ,val);
-        return val;
-     }
-   });
-//    console.log(filteredData);
-  if(filteredData.length === 0){
-    table.innerText = "Nothing Found!"
+    if (
+      stName.includes(curr) ||
+      stEmail.includes(curr) ||
+      stDegree.includes(curr)
+    ) {
+      // console.log("from filter fn " ,val);
+      return val;
+    }
+  });
+  //    console.log(filteredData);
+  if (filteredData.length === 0) {
+    table.innerText = "Nothing Found!";
     return;
   }
-   renderSearch(filteredData);
-}
+  renderSearch(filteredData);
+};
 
 const renderSearch = (filteredData) => {
-    let toEnterTable = "";
-    // table.innerHTML = "";
-    filteredData.map((val) => {
+  let toEnterTable = "";
+  // table.innerHTML = "";
+  filteredData.map((val) => {
     toEnterTable += `
                 <tr>
-                <td>${val['id']}</td>
-                <td>${val['name']}</td>
+                <td>${val["id"]}</td>
+                <td>${val["name"]}</td>
                 <td>${val["email"]}</td>
                 <td>${val["age"]}</td>
                 <td>${val["gpa"]}</td>
-                <td class="table-body-btn">${val["degree"]} <button value=${val['id']} onClick="editBtnHandler(this)" id="edit-btn"></button> <button onClick="deleteStudentBtnHandler(this)" value=${val['id']} id="trash-btn"></button></td>
+                <td class="table-body-btn">${val["degree"]} <button value=${val["id"]} onClick="editBtnHandler(this)" id="edit-btn"></button> <button onClick="deleteStudentBtnHandler(this)" value=${val["id"]} id="trash-btn"></button></td>
             <tr>
-    `
-});
-   table.innerHTML = toEnterTable;
-}
-
-
+    `;
+  });
+  table.innerHTML = toEnterTable;
+};
 
 // editing from table
 const editBtnHandler = (btnValue) => {
-    const id = btnValue.value;
-    // console.log(id);
-    addStudentBtn.style.display = 'none';
-    editStudentBtn.style.display = "flex";
-    editStudentBtn.style.justifyContent = "center";
-    
-    
-    studentDetails.forEach((val) => {
-        if(val["id"] === id){
-            // console.log(val);
-            row = val;
-        }
-    });
+  const id = btnValue.value;
+  // console.log(id);
+  addStudentBtn.style.display = "none";
+  editStudentBtn.style.display = "flex";
+  editStudentBtn.style.justifyContent = "center";
 
-    editForm();
-    // console.log(row);
-}
+  studentDetails.forEach((val) => {
+    if (val["id"] === id) {
+      // console.log(val);
+      row = val;
+    }
+  });
+
+  editForm();
+  // console.log(row);
+};
 
 const editForm = () => {
-    studentInputName.value = row["name"];
-    studentInputEmail.value = row["email"];
-    studentInputGpa.value = row["gpa"];
-    studentInputAge.value = row["age"];
-    studentInputDegree.value = row["degree"];
-}
-
-
+  studentInputName.value = row["name"];
+  studentInputEmail.value = row["email"];
+  studentInputGpa.value = row["gpa"];
+  studentInputAge.value = row["age"];
+  studentInputDegree.value = row["degree"];
+};
 
 // adding into table
 
 const insertIntoTable = () => {
-    let toEnterTable = ""
-    // table.innerHTML = "";
-    studentDetails.map((val) => {
+  let toEnterTable = "";
+  // table.innerHTML = "";
+  studentDetails.map((val) => {
     toEnterTable += `
                 <tr>
-                <td>${val['id']}</td>
-                <td>${val['name']}</td>
+                <td>${val["id"]}</td>
+                <td>${val["name"]}</td>
                 <td>${val["email"]}</td>
                 <td>${val["age"]}</td>
                 <td>${val["gpa"]}</td>
-                <td class="table-body-btn">${val["degree"]} <button value=${val['id']} onClick="editBtnHandler(this)" id="edit-btn"></button> <button onClick="deleteStudentBtnHandler(this)" value=${val['id']} id="trash-btn"></button></td>
+                <td class="table-body-btn">${val["degree"]} <button value=${val["id"]} onClick="editBtnHandler(this)" id="edit-btn"></button> <button onClick="deleteStudentBtnHandler(this)" value=${val["id"]} id="trash-btn"></button></td>
             <tr>
-    `
-});
-   table.innerHTML = toEnterTable;
-}
-
-
-
-
+    `;
+  });
+  table.innerHTML = toEnterTable;
+};
 
 // edit student btn handler
 
-function editStudentBtnHandler(ev){
-    ev.preventDefault();
-    if(row["age"] <5 || row["age"] > 100){
-        alert("please enter valid age!!")
-        return;
-    }
-    if(row["gpa"] <0 || row["gpa"] > 10){
-        alert("please enter valid gpa between 1 and 10!!")
-        return;
-    }
-    
-    row['name'] = studentInputName.value;
-    row['age'] = studentInputAge.value;
-    row["email"] = studentInputEmail.value;
-    row["gpa"] = parseFloat(studentInputGpa.value).toFixed(2);
-    row["degree"]= studentInputDegree.value;
-    // let id = "id" + Math.random().toString(16).slice(2);
+function editStudentBtnHandler(ev) {
+  ev.preventDefault();
+  if (row["age"] < 5 || row["age"] > 100) {
+    alert("please enter valid age!!");
+    return;
+  }
+  if (row["gpa"] < 0 || row["gpa"] > 10) {
+    alert("please enter valid gpa between 1 and 10!!");
+    return;
+  }
 
-    // const details = {
-    //     id: id,
-    //     name: studentName,
-    //     email: studentEmail,
-    //     gpa: studentGpa,
-    //     age: studentAge,
-    //     degree: studentDegree,
-    // }
-    // studentDetails.push(details);
-    studentInputName.value = "";
-    studentInputAge.value = "";
-    studentInputEmail.value = "";
-    studentInputGpa.value = "";
-    studentInputDegree.value = "";
-    console.log("after editing",studentDetails);
-    addStudentBtn.style.display = 'flex';
-    addStudentBtn.style.justifyContent = "center";
-    editStudentBtn.style.display = "none";
-    row = {};
-    insertIntoTable();
+  row["name"] = studentInputName.value;
+  row["age"] = studentInputAge.value;
+  row["email"] = studentInputEmail.value;
+  row["gpa"] = parseFloat(studentInputGpa.value).toFixed(2);
+  row["degree"] = studentInputDegree.value;
+  // let id = "id" + Math.random().toString(16).slice(2);
+
+  // const details = {
+  //     id: id,
+  //     name: studentName,
+  //     email: studentEmail,
+  //     gpa: studentGpa,
+  //     age: studentAge,
+  //     degree: studentDegree,
+  // }
+  // studentDetails.push(details);
+  studentInputName.value = "";
+  studentInputAge.value = "";
+  studentInputEmail.value = "";
+  studentInputGpa.value = "";
+  studentInputDegree.value = "";
+  console.log("after editing", studentDetails);
+  addStudentBtn.style.display = "flex";
+  addStudentBtn.style.justifyContent = "center";
+  editStudentBtn.style.display = "none";
+  row = {};
+  insertIntoTable();
 }
-
 
 // delete student btn handler
 const deleteStudentBtnHandler = (obj) => {
-    const id = obj['value'];
-    const afterDelete = studentDetails.filter((val) => {
-        if(val["id"] !== id){
-            return val;
-        }
-    })
-    studentDetails = afterDelete;
-    insertIntoTable();
-}
-
-
+  const id = obj["value"];
+  const afterDelete = studentDetails.filter((val) => {
+    if (val["id"] !== id) {
+      return val;
+    }
+  });
+  studentDetails = afterDelete;
+  insertIntoTable();
+};
 
 // capturing student details
 
-function formDataHandler(ev){
-    ev.preventDefault();
-    const studentName = studentInputName.value;
-    const studentAge = studentInputAge.value;
-    const studentEmail = studentInputEmail.value;
-    const studentGpa = parseFloat(studentInputGpa.value).toFixed(2);
-    const studentDegree = studentInputDegree.value;
-    if(studentAge <5 || studentAge > 100){
-        alert("please enter valid age!!")
-        return;
-    }
-    if(studentGpa <0 || studentGpa > 10){
-        alert("please enter valid gpa between 1 and 10!!")
-        return;
-    }
-    
-    let id = "id" + Math.random().toString(16).slice(2);
+function formDataHandler(ev) {
+  ev.preventDefault();
+  const studentName = studentInputName.value;
+  const studentAge = studentInputAge.value;
+  const studentEmail = studentInputEmail.value;
+  const studentGpa = parseFloat(studentInputGpa.value).toFixed(2);
+  const studentDegree = studentInputDegree.value;
+  if (studentAge < 5 || studentAge > 100) {
+    alert("please enter valid age!!");
+    return;
+  }
+  if (studentGpa < 0 || studentGpa > 10) {
+    alert("please enter valid gpa between 1 and 10!!");
+    return;
+  }
 
-    const details = {
-        id: id,
-        name: studentName,
-        email: studentEmail,
-        gpa: studentGpa,
-        age: studentAge,
-        degree: studentDegree,
-    }
-    studentDetails.push(details);
-    studentInputName.value = "";
-    studentInputAge.value = "";
-    studentInputEmail.value = "";
-    studentInputGpa.value = "";
-    studentInputDegree.value = "";
-    console.log("before editing",studentDetails);
-    insertIntoTable();
+  let id = "id" + Math.random().toString(16).slice(2);
+
+  const details = {
+    id: id,
+    name: studentName,
+    email: studentEmail,
+    gpa: studentGpa,
+    age: studentAge,
+    degree: studentDegree,
+  };
+  studentDetails.push(details);
+  studentInputName.value = "";
+  studentInputAge.value = "";
+  studentInputEmail.value = "";
+  studentInputGpa.value = "";
+  studentInputDegree.value = "";
+  console.log("before editing", studentDetails);
+  insertIntoTable();
 }
 
 
-form.addEventListener('submit', formDataHandler);
-editStudentBtn.addEventListener('click', editStudentBtnHandler);
-search.addEventListener('keyup', searchHandler);
-
-
+// handling user events
+form.addEventListener("submit", formDataHandler);
+editStudentBtn.addEventListener("click", editStudentBtnHandler);
+search.addEventListener("keyup", searchHandler);
